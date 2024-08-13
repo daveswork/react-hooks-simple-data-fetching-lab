7e3fe4abcbd9a63d66f4716441928290ecc1a17c
Dave J <29786887+daveswork@users.noreply.github.com>
2024-08-13 17:18:31 -0400

    All local tests pass.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+function App(){
+    const [dogImage, setDogImage] = useState(<p>Loading...</p>)
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            setDogImage(<img src={data.message} alt="A Random Dog"/>)
+        })
+    }, [])
+
+    return (
+        dogImage
+    )
+
+
+}
+
+
+export default App
